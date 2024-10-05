8e8c39b74a00e805b080828263f7c503c9a18841
saleheen1 <smsaleheen3@gmail.com>
2024-10-05 15:27:43 +0600

    sign up working

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_twitter_app/core/constants/appwrite_constants.dart';
 import 'package:flutter_twitter_app/core/routes/app_router.dart';
 import 'package:flutter_twitter_app/core/theme/app_theme.dart';
 import 'package:flutter_twitter_app/features/auth/views/login_view.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
